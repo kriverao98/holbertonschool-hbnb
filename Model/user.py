a8113f718a76251ffa152bb70c5b3645fe658a48
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-06 01:19:07 -0400

    created place and user class

---
+++ Model/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is an user class for the Airbnb project"""
+
+class User:
+    """Defining user class attributes"""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    user_id = ""
+    created_at = ""
+    updated_at = ""
